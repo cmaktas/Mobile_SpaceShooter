efd978cf01c20ff5a2d645e219f34e16587c714c
Cem Aktas <75162943+cmaktas@users.noreply.github.com>
2025-08-31 10:37:58 +0300

    optimized and pooled player bullets

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,12 +1,14 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] private GameObject laserBullet;
+    [SerializeField] private PlayerLaserBullet laserBullet;
     [SerializeField] private float shootingInterval;
 
     [Header("Basic Canon")]
-    [SerializeField] private Transform basicCanonPoint;
+    [SerializeField] private Transform basicCanon;
 
     [Header("Upgrade Attack Standart Canons")]
     [SerializeField] private Transform leftCanon;
@@ -20,17 +22,33 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField] private Transform secondLeftRotatedCanon;
     [SerializeField] private Transform secondRightRotatedCanon;
 
+    [Header("Audio")]
     [SerializeField] private AudioSource audioSource;
 
     private int upgradeCanonLevel;
     private float intervalReset;
+    private ObjectPool<PlayerLaserBullet> laserBulletPool;
+
+    private static readonly int[] StandardCountPerLevel = { 1, 3, 5, 5, 5 };
+    private static readonly int[] RotatedCountPerLevel  = { 0, 0, 0, 2, 4 };
+
+    private Transform[] standardOrder;
+    private Transform[] rotatedOrder;
+
+    void Awake()
+    {
+        laserBulletPool = new ObjectPool<PlayerLaserBullet>(CreateLaserBullet, TakeBulletFromPool,
+                                                            ReturnBulletFromPool, DestroyBulletFromPool,
+                                                            true, 10, 30);
+        standardOrder = new Transform[] { basicCanon, leftCanon, rightCanon, secondLeftCanon, secondRightCanon };
+        rotatedOrder  = new Transform[] { leftRotatedCanon, rightRotatedCanon, secondLeftRotatedCanon, secondRightRotatedCanon };
+    }
 
     void Start()
     {
         intervalReset = shootingInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
         shootingInterval -= Time.deltaTime;
@@ -62,43 +80,66 @@ public class PlayerShooting : MonoBehaviour
     private void Shoot()
     {
         audioSource.Play();
-        switch (upgradeCanonLevel)
+
+        int standardToFire = StandardCountPerLevel[upgradeCanonLevel];
+        int rotatedToFire  = RotatedCountPerLevel[upgradeCanonLevel];
+
+        FireStandard(standardToFire);
+        FireRotated(rotatedToFire);
+    }
+
+    private void FireStandard(int count)
+    {
+        for (int i = 0; i < count && i < standardOrder.Length; i++)
         {
-            case 0:
-                Instantiate(laserBullet, basicCanonPoint.position, Quaternion.identity);
-                break;
-            case 1:
-                Instantiate(laserBullet, basicCanonPoint.position, Quaternion.identity);
-                Instantiate(laserBullet, leftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, rightCanon.position, Quaternion.identity);
-                break;
-            case 2:
-                Instantiate(laserBullet, basicCanonPoint.position, Quaternion.identity);
-                Instantiate(laserBullet, leftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, rightCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, secondLeftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, secondRightCanon.position, Quaternion.identity);
-                break;
-            case 3:
-                Instantiate(laserBullet, basicCanonPoint.position, Quaternion.identity);
-                Instantiate(laserBullet, leftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, rightCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, secondLeftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, secondRightCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, leftRotatedCanon.position, leftRotatedCanon.rotation);
-                Instantiate(laserBullet, rightRotatedCanon.position, rightRotatedCanon.rotation);
-                break;
-            case 4:
-                Instantiate(laserBullet, basicCanonPoint.position, Quaternion.identity);
-                Instantiate(laserBullet, leftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, rightCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, secondLeftCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, secondRightCanon.position, Quaternion.identity);
-                Instantiate(laserBullet, leftRotatedCanon.position, leftRotatedCanon.rotation);
-                Instantiate(laserBullet, rightRotatedCanon.position, rightRotatedCanon.rotation);
-                Instantiate(laserBullet, secondLeftRotatedCanon.position, secondLeftRotatedCanon.rotation);
-                Instantiate(laserBullet, secondRightRotatedCanon.position, secondRightRotatedCanon.rotation);
-                break;
+            if (standardOrder[i] != null)
+            {
+                InstantiateBullet(standardOrder[i], false);
+            }
         }
+    }
+
+    private void FireRotated(int count)
+    {
+        for (int i = 0; i < count && i < rotatedOrder.Length; i++)
+        {
+            if (rotatedOrder[i] != null)
+            {
+                InstantiateBullet(rotatedOrder[i], true);
+            }
+        }
+    }
+
+    private void InstantiateBullet(Transform canon, bool isRotated)
+    {
+        PlayerLaserBullet playerLaserBullet = laserBulletPool.Get();
+        playerLaserBullet.transform.position = canon.position;
+        if (isRotated)
+        {
+            playerLaserBullet.transform.rotation = canon.rotation;
+            playerLaserBullet.SetDirectionAndSpeed();
+        }
+    }
+    
+    private PlayerLaserBullet CreateLaserBullet()
+    {
+        PlayerLaserBullet bullet = Instantiate(laserBullet, transform.position, transform.rotation);
+        bullet.SetPool(laserBulletPool);
+        return bullet;
+    }
+
+    private void TakeBulletFromPool(PlayerLaserBullet laserBullet)
+    {
+        laserBullet.gameObject.SetActive(true);
+    }
+
+    private void ReturnBulletFromPool(PlayerLaserBullet laserBullet)
+    {
+        laserBullet.gameObject.SetActive(false);
+    }
+
+    private void DestroyBulletFromPool(PlayerLaserBullet laserBullet)
+    {
+        Destroy(laserBullet.gameObject);
     }
 }
